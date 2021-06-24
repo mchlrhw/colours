61a1d76b2b5fc869b617d1713bd61c3d6a55c5c3
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2021-06-24 10:19:07 +0100

    Actually use Image data

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,31 @@
-struct Image {}
+struct Colour {
+    r: u8,
+    g: u8,
+    b: u8,
+}
 
-fn image_to_ppm(_image: Image) -> String {
-    String::from(
-        r#"P3
-3 3
-255
-0 0 0
-0 0 0
-0 0 0
-0 0 0
-0 0 0
-0 0 0
-0 0 0
-0 0 0
-0 0 0"#,
-    )
+struct Image {
+    width: u8,
+    height: u8,
+    pixels: Vec<Colour>,
+}
+
+fn image_to_ppm(image: Image) -> String {
+    let mut ppm = format!("P3\n{} {}\n255\n", image.width, image.height);
+
+    for pixel in image.pixels {
+        ppm += &format!("{} {} {}\n", pixel.r, pixel.g, pixel.b);
+    }
+
+    ppm
 }
 
 fn main() {
-    let image = Image {};
+    let image = Image {
+        width: 1,
+        height: 1,
+        pixels: vec![Colour { r: 0, g: 0, b: 0 }],
+    };
     let ppm = image_to_ppm(image);
     println!("{}", ppm);
 }
