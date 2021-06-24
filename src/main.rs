ece67b8790659f65721e320746108c8601af4b9f
mchlrhw <4028654+mchlrhw@users.noreply.github.com>
2021-06-24 10:17:54 +0100

    Create the Image type

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn main() {
-    let ppm = r#"P3
+struct Image {}
+
+fn image_to_ppm(_image: Image) -> String {
+    String::from(
+        r#"P3
 3 3
 255
 0 0 0
@@ -10,7 +13,12 @@ fn main() {
 0 0 0
 0 0 0
 0 0 0
-0 0 0"#;
+0 0 0"#,
+    )
+}
 
+fn main() {
+    let image = Image {};
+    let ppm = image_to_ppm(image);
     println!("{}", ppm);
 }
